e3c2ac572e5ca6bb692a6742ae1d0c0160078b47
Durjoy-dcp <unknownccxv@gmail.com>
2022-11-07 11:20:59 +0600

    data collection id update for mongodb

--- src/Components/Shared/AllCatagory/AllCatagory.js
+++ src/Components/Shared/AllCatagory/AllCatagory.js
@@ -7,7 +7,7 @@ import CatagoryName from '../CatagoryName/CatagoryName';
 const AllCatagory = () => {
     const [allCatagory, setAllCatagory] = useState([]);
     useEffect(() => {
-        fetch('https://learnap-server-durjoy-dcp.vercel.app/catagory')
+        fetch('http://localhost:5000/catagory')
             .then(res => res.json())
             .then(data => setAllCatagory(data));
     }, []);
